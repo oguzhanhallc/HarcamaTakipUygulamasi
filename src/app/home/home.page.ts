fce5809ed49160221d88588a746803cc4d8041c6
oguzhanhallc <oguzhanhallc@gmail.com>
2026-05-05 16:45:22 +0300

    Add files via upload

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage implements OnInit {
+
+  harcamaAdi: string = "";
+  harcamaTutari: number | null = null;
+  kategori: string = "";
+  harcamalar: any[] = [];
+  HarcamaToplam : number = 0;
+
+  constructor() {}
+
+  ngOnInit() {
+  const veriler = localStorage.getItem('harcamalar');
+  if (veriler) {
+    this.harcamalar = JSON.parse(veriler);
+    
+    // Sayfa açılırken toplamı hemen hesapla ki 0 görünmesin!
+    this.HarcamaToplam = 0;
+    for (let h of this.harcamalar) {
+      this.HarcamaToplam += Number(h.harcamaTutari);
+    }
+  }
+}
+
+  Kaydet() {
+    if (!this.harcamaAdi || this.harcamaTutari === null || !this.kategori) {
+      alert("Lütfen tüm alanları doldurun!");
+      return;
+    }
+    console.log("Harcama Adı: " + this.harcamaAdi);
+    console.log("Harcama Tutarı: " + this.harcamaTutari);
+    console.log("Kategori: " + this.kategori);
+    this.harcamalar.push({
+      harcamaAdi: this.harcamaAdi,
+      harcamaTutari: this.harcamaTutari,
+      kategori: this.kategori
+    });
+    this.HarcamaToplamHesapla();
+    localStorage.setItem('harcamalar', JSON.stringify(this.harcamalar));
+    this.harcamaAdi = "";
+    this.harcamaTutari = null;
+    this.kategori = "";
+  }
+
+  HarcamaToplamHesapla() {
+    let toplam = 0;
+    for (let harcama of this.harcamalar) {
+      toplam += harcama.harcamaTutari;
+    }
+    this.HarcamaToplam = toplam;
+  }
+
+  Sil(index: number) {
+    this.harcamalar.splice(index, 1);
+    this.HarcamaToplamHesapla();
+  }
+
+  SaveToLocalStorage() {
+    localStorage.setItem('harcamalar', JSON.stringify(this.harcamalar));
+  }
+}
